dc422c9b9a48ed60febcfa1ef7d201e70888182c
Sebastian Skuza <sebastian.skuzza@gmail.com>
2017-08-24 14:55:21 +0200

    Add name of the game

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 def import_map(filename):
     imported_list = []
-    flatted_list = []
 
     for item in open(filename):
         item = item.strip('\n')
@@ -10,8 +9,21 @@ def import_map(filename):
         print(''.join(n))
 
 
-def main():
+def show_title(filename):
+    imported_list = []
 
+    for item in open(filename):
+        item = item.strip('\n')
+        imported_list.append(item)
+
+    for n in imported_list:
+        print(''.join(n))
+
+
+
+
+def main():
+    show_title('game_title.txt')
     import_map('game_board.txt')
 
 main()
